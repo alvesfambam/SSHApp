481f8140ae155f665f04d05d38f6ada1231758a6
Ryan <alvesfambam@gmail.com>
2026-05-04 18:00:28 -0700

    fix

--- obj/Debug/net8.0-windows/SSHApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SSHApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SSHApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+650269b29e50779f0705dcf05939e2bd44478220")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3053986887ff66260026b23c51f8b809736dd2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SSHApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SSHApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
